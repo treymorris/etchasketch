23b725f43b56e206ba68617b1efa3129ac8d95b1
Trey Morris <treymorrisit@hotmail.com>
2021-10-08 12:59:21 -0400

    helps to read instructions now have 16 divs per side to start and aded header

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //add 16 divs to the dom
 var container = document.querySelector('#container');
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
 
     const grids = document.createElement('div');
     grids.classList.add('grids');
-    grids.style.backgroundColor = 'blue';
+    grids.style.backgroundColor = 'white';
     container.appendChild(grids);
     grids.addEventListener('mouseover', runEvent); //set up hover effect to make divs change color
     function runEvent(e) {
